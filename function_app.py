6b614f520f734fafdb889cc4e7c26e61ca58b07e
anuthedeveloper <tunsabaze01@gmail.com>
2024-12-06 08:49:25 +0000

    Zip file added

--- function_app.py
+++ function_app.py
@@ -8,21 +8,18 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
+    try:
+        url = 'https://burntlettuce.azurewebsites.net/api/http_trigger1?code=nB4JIZZcCKVj6gfNqF1gh5iaDe88iZ34NMBTvPEIEY7pAzFu_2zGrg%3D%3D'
+        # url = 'https://api.github.com/users'
+        response = requests.get(url)
+
+        if response:
+            return func.HttpResponse(f"Hello, {response.json()}. This HTTP triggered function executed successfully.")
         else:
-            name = req_body.get('name')
-
-    response = requests.get('https://api.github.com/users')
-
-    if response:
-        return func.HttpResponse(f"Hello, {response.json()}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+            return func.HttpResponse(
+                "This HTTP triggered function executed successfully. Pass a response in the query string or in the request body for a personalized response.",
+                status_code=200
+            )
+    except Exception as e:
+        logging.error(f"Error processing response: {str(e)}")
+        return func.HttpResponse(f"Error: {str(e)}", status_code=500)
